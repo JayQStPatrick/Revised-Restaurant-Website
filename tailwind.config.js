33512712c542e0a04aabe2bb55ec0e3452e20f3b
JayQStPatrick <jqurix352@gmail.com>
2024-06-10 19:27:41 -0700

    test commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffc001",
+        secondary: "#ff9c01",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
